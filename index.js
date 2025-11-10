2afef0f9b1a3a8e7250958519b6e8096331063d9
sorkar5sourav <sorkar.s.sourav@gmail.com>
2025-11-10 14:15:53 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri =
+  "mongodb+srv://Paw-Mart-admin:NDkReapXwKPdMobB@cluster0.dddm4vc.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("Paw-Mart-Listing");
+    const listingsCollection = database.collection("Listings");
+
+    app.get("/listings", async (req, res) => {
+      const result = await listingsCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("🚀 Server is running...Just Fine");
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`✅ Server running on old port ${port}`);
 });
+
+//
